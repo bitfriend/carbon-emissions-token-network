c7dab85f5f8430701017214a8387e8e10c3baab2
bitfriend <bitfriend@hotmail.com>
2021-05-13 21:51:27 +0200

    Updates

---
+++ precache-manifest.b067d80571727212695c4dde21f4ec39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4567576f2cfbf131bc2e09c08729f1de",
+    "url": "/carbon-emissions-token-network/index.html"
+  },
+  {
+    "revision": "4bb04aa211948fbf5c3a",
+    "url": "/carbon-emissions-token-network/static/css/2.cf62996a.chunk.css"
+  },
+  {
+    "revision": "cb22bef8029809f3872e",
+    "url": "/carbon-emissions-token-network/static/css/main.6569f3b6.chunk.css"
+  },
+  {
+    "revision": "4bb04aa211948fbf5c3a",
+    "url": "/carbon-emissions-token-network/static/js/2.4bf5202a.chunk.js"
+  },
+  {
+    "revision": "d76341a167b45a0386ef85ae8a3e5577",
+    "url": "/carbon-emissions-token-network/static/js/2.4bf5202a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb22bef8029809f3872e",
+    "url": "/carbon-emissions-token-network/static/js/main.ce0d192a.chunk.js"
+  },
+  {
+    "revision": "4971685c5d53b8644265",
+    "url": "/carbon-emissions-token-network/static/js/runtime-main.ef148037.js"
+  }
+]);
